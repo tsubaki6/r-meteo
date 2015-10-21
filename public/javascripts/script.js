29afb3cb27c04f6a0e49b5c9cebfe98dd504eafb
unknown <martyna.jablonska7@gmail.com>
2015-10-21 19:59:56 +0200

    Sparsowane dane z wundergorund

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,13 +5,61 @@ app.controller('MainCtrl', [
 function($scope,$http){
   $scope.x = 'Hello world!';
   $scope.dane="a";
+  var wunderground = [];
+  var i,j,days, temperature, wind, qpf;
   $http.get('http://api.wunderground.com/api/6cdc9ccbd68485fe/forecast10day/q/Poland/Krakow.json')
   .success(function(data){
         $scope.dane = data;
+        console.log(Object.keys($scope.dane));
+        var danesz = data["forecast"]["simpleforecast"]["forecastday"];
+        //console.log(data);
+        for(i=0;i<danesz.length;i++){
+          var forecast = danesz[i];
+          days = daysCount(forecast["date"]["day"], forecast["date"]["month"]);
+          qpf = forecast["qpf_allday"]["mm"];
+          var min, max;
+          min = forecast["low"]["celsius"];
+          max = forecast["high"]["celsius"];
+          temperature = (parseInt(min)+parseInt(max))/2;
+          wind = forecast["avewind"]["mph"]/3600;
+//          console.log(wind);
+//          console.log(forecast);
+          wunderground[i] = {"days":days, "temperature":temperature, "qpf":qpf, "wind":wind}
+        }
+    console.log(wunderground);
   })
   .error(function(){
     console.log('BLAD');
   })
+
+  var daysCount = function(forecastDay, month){
+    var data = new Date();
+    var day = data.getDate()
+    var count;
+    //console.log(data.getDate());
+    if(day<forecastDay){
+      count = forecastDay-day;
+    }
+    else{
+      switch(month){
+      case 1,3,5,7,8,10,12: {
+        count=31-day+forecastDay;
+        break;
+      }
+      case 4,6,9,11: {
+        count=30-day+forecastDay;
+        break;
+       }
+      case 2: {
+        count=28-day+forecastDay;
+        break;
+      }
+      }
+    }
+    //console.log(count);
+    return count;
+  }
+     daysCount(2,2);
 
     $http.get('http://pogoda.interia.pl/prognoza-dlugoterminowa-krakow,cId,4970')
     .success(function(data){
@@ -20,24 +68,10 @@ function($scope,$http){
     .error(function(){
       console.log('BLAD');
     })
-//  $http.get('api.yr.no/weatherapi/locationforecast/1.9/?lat=60.10;lon=9.58',
-//  {transformResponse:function(data) {
-//                      // convert the data to JSON and provide
-//                      // it to the success function below
-//                        //var x2js = new X2JS();
-//                        var json = x2js.xml_str2json( data );
-//                        return json;
-//                        }
-//                    })
-//  .success(function(data, callback){
-//		callback();
-//		console.log("aaa");
-//  })
-//  .error(function(){
-//	  console.log('blad');
-//  });
-//	var callback = function(){
-//		$scope.dane = data;
-//		console.log("AA");
-//	}
+
+  $http.get('http://api.yr.no/weatherapi/locationforecast/1.9/?lat=50.04;lon=19.57')
+  .success(function(data){
+        $scope.dane3 = data;
+  })
+
 }]);
