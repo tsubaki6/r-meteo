1dd7ef2ee2620f3c4582c5e6b055c9c80fe606a6
Sakuraa <sakuraa321@gmail.com>
2015-10-22 18:14:37 +0200

    Sparsowane dane z interii

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,6 +6,7 @@ function($scope,$http){
   $scope.x = 'Hello world!';
   $scope.dane="a";
   var wunderground = [];
+  var interia = [];
   var i,j,days, temperature, wind, qpf;
   $http.get('http://api.wunderground.com/api/6cdc9ccbd68485fe/forecast10day/q/Poland/Krakow.json')
   .success(function(data){
@@ -16,12 +17,13 @@ function($scope,$http){
         for(i=0;i<danesz.length;i++){
           var forecast = danesz[i];
           days = daysCount(forecast["date"]["day"], forecast["date"]["month"]);
+          //console.log(days);
           qpf = forecast["qpf_allday"]["mm"];
           var min, max;
           min = forecast["low"]["celsius"];
           max = forecast["high"]["celsius"];
           temperature = (parseInt(min)+parseInt(max))/2;
-          wind = forecast["avewind"]["mph"]/3600;
+          wind = forecast["avewind"]["kph"];//3600;
 //          console.log(wind);
 //          console.log(forecast);
           wunderground[i] = {"days":days, "temperature":temperature, "qpf":qpf, "wind":wind}
@@ -64,6 +66,21 @@ function($scope,$http){
     $http.get('http://pogoda.interia.pl/prognoza-dlugoterminowa-krakow,cId,4970')
     .success(function(data){
           $scope.dane2 = data;
+          var parser = new DOMParser().parseFromString(data,"text/html");
+          var forecast = parser.getElementsByClassName("weather-forecast-longterm-list")[0];
+          var min, max;
+         for(i=0;i<=7;i++){
+            var data = forecast.getElementsByClassName("date")[i].innerHTML.split('.',2);
+            days = daysCount(parseInt(data[0]),parseInt(data[1]));
+            max = forecast.getElementsByClassName("weather-forecast-longterm-list-entry-forecast-temp")[i].innerHTML;
+            min = forecast.getElementsByClassName("weather-forecast-longterm-list-entry-forecast-lowtemp")[i].innerHTML;
+            temperature = (parseInt(min)+parseInt(max))/2;
+            wind = parseInt(forecast.getElementsByClassName("weather-forecast-longterm-list-entry-wind-value")[i].innerHTML);
+            qpf = parseFloat(forecast.getElementsByClassName("weather-forecast-longterm-list-entry-precipitation-value")[i].innerHTML.replace(",","."));
+            interia[i] = {"days":days, "temperature":temperature, "qpf":qpf, "wind":wind}
+         }
+          console.log(interia);
+
     })
     .error(function(){
       console.log('BLAD');
@@ -73,5 +90,13 @@ function($scope,$http){
   .success(function(data){
         $scope.dane3 = data;
   })
+
+  $http.get('http://meteo.ftj.agh.edu.pl/meteo/meteo.xml')
+      .success(function(data){
+        $scope.meteo = data;
+      })
+      .error(function(){
+        console.log('BLAD');
+      })
 
 }]);
